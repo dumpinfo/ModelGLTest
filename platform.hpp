215cc4b36a907e48ff8c4af17fc55914753ee68e
speech <wang_tian_zhou@163.com>
2019-05-22 16:41:41 +0800

    test

---
+++ platform.hpp
@@ -0,0 +1,11 @@
+//
+// Licensed under the terms in LICENSE.txt
+//
+// Copyright 2014 Tom Dalling. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+
+inline std::string ResourcePath(std::string fileName){return  std::string("../resources/")+fileName;};
---
+++ source/mk.sh
@@ -0,0 +1,1 @@
+g++ *.cpp tdogl/*.cpp -I../../../  -o main  -lGL  -lGLEW -lglut -lglfw
